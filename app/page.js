87f3bec7b2b460452711893000e56d4685e7fab3
Juan11211 <avilesjuan628@gmail.com>
2023-11-18 23:22:15 -0500

    working on some ui designs. will start w/ backend soon

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Profile from './profile/page'
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Profile />
+
+    </main>
+  )
+}
---
+++ app/profile/page.js
@@ -0,0 +1,13 @@
+"use client"
+
+import React from 'react'
+
+function Profile() {
+  return (
+    <div>
+      HELLO
+    </div>
+  )
+}
+
+export default Profile

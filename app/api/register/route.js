9411dc19d546cd552a4e49d9521a09bdfe1b7b06
Juan11211 <avilesjuan628@gmail.com>
2023-11-23 22:03:52 -0500

    signup route working, tested in postman

---
+++ app/api/register/route.js
@@ -0,0 +1,41 @@
+import connect from "../../lib/db";
+import User from "../../../models/User"; // Correct import
+import bcrypt from 'bcryptjs'
+import { NextResponse } from "next/server";
+
+export const POST = async (request) => {
+  const { username, email, password } = await request.json();
+
+  await connect();
+
+  const existingUsername = await User.findOne({ username });
+
+  const existingEmail = await User.findOne({ email });
+
+  if (existingUsername) {
+    return new NextResponse(`Username is already taken`);
+  }
+
+  if (existingEmail) {
+    return new NextResponse(`Email is already taken`);
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await newUser.save();
+
+    return new NextResponse('User is registered', {
+      status: 200,
+    });
+  } catch (err) {
+    return new NextResponse(`This is not connecting`, {
+      status: 500,
+    });
+  }
+};
--- app/lib/db.js
+++ app/lib/db.js
@@ -1,15 +1,16 @@
 import mongoose from 'mongoose';
 
-const connect = async() => {
-    try{
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        console.log("Mongo Connected!")
-    } catch(error) {
-        throw new Error('Error connecting to mongodb')
-    }
-}
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+   
+    });
+    console.log('Mongo Connected!');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+    throw new Error('Error connecting to MongoDB');
+  }
+};
 
 export default connect;
